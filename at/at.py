f635003d210002ebe8a2cd1c3a2f9a68d98783a2
Matt Barlow <matt.barlow@rackspace.com>
2016-04-21 13:56:22 +0000

    Update swagger

--- at/at.py
+++ at/at.py
@@ -1,10 +1,12 @@
 import logging
+import pprint
 
 
 LOG = logging.getLogger()
 logging.basicConfig()
 LOG.setLevel(logging.INFO)
+pp = pprint.PrettyPrinter(width=300)
 
 
 def lambda_handler(event, context):
-    LOG.info('received event{}'.format(event))
+    pp.pprint(event)
